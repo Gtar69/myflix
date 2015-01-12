3fb3728220fd5a59c2c6d0175ea43096fa80214e
Gtar <chihweichang69@gmail.com>
2015-01-12 17:40:25 +0800

    done with pre course

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+family_guy = Video.create(title: "family guy", description: 
+	"funny episode", small_cover_url: "family_guy",
+	large_cover_url: "family_guy")
+south_park = Video.create(title: "south park", description: 
+	"south park", small_cover_url: "south_park",
+	large_cover_url: "south_park")
+
+cat_1 = Category.create(name: "TV Commodies")
+cat_2 = Category.create(name: "TV show")
+
+cat_1.videos << family_guy
