3e5ff5c0fe050cf307f5c511a4ed6f5170e0b88e
Gtar <chihweichang69@gmail.com>
2015-01-13 18:08:56 +0800

    add rspec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:videos)}
+
+  #it "save itself" do
+  #  category = Category.new(name: "comedies")
+  #  category.save
+  #  expect(Category.first).to eq(category)
+  #end
+
+  #pending "add some examples to (or delete) #{__FILE__}"
+  #it "has many videos" do 
+  #  cat_01 = Category.create(name: "TV Commodies")
+  #  v_01 = Video.create(title: "family guy",description: 
+  #    "family guy test! ! !", category: cat_01)
+  #  v_02 = Video.create(title: "south park",description: 
+  #    "south park test! ! !", category: cat_01)
+  #  expect(cat_01.videos).to include(v_01, v_02)
+  #end
 end
