e68995d95afd25020b8459d9f17bbdcf38322533
Gtar <chihweichang69@gmail.com>
2015-01-20 16:57:51 +0800

    modify videos_controller test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120061506) do
+ActiveRecord::Schema.define(version: 20150120082407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150120061506) do
 
   create_table "reviews", force: true do |t|
     t.integer  "rating"
-    t.text     "review"
+    t.text     "content"
     t.integer  "user_id"
     t.integer  "video_id"
     t.datetime "created_at"
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,33 +3,36 @@ require 'spec_helper'
 describe VideosController do
   
   describe "GET show" do
-    it "set the @video variable" do
-      family_guy = Video.create(title: "family guy", description: "funny funny")
-      south_park = Video.create(title: "south park", description: "south park" )
-      get :show, id: south_park.id
-      assigns(:video).should == south_park 
+    before do
+      session[:user_id] = Fabricate(:user).id
+      #8.times {Fabricate(:video)}
+      @video = Fabricate(:video)
     end
 
+    it "sets the @video variable" do
+      get :show, id: @video.id
+      assigns(:video).should == @video 
+    end
+
+    it "set the @reviews variables"
+
     it "render the show template" do
-      family_guy = Video.create(title: "family guy", description: "funny funny")
-      south_park = Video.create(title: "south park", description: "south park" )
-      get :show, id: south_park.id 
+      get :show, id: @video.id 
       response.should render_template :show
     end
     
   end
   
   describe "GET search" do
-    it "set the @videos variable" do
-      family_guy = Video.create(title: "family guy", description: "funny funny")
-      south_park = Video.create(title: "south park", description: "south park" )
-      family_guy_2 = Video.create(title: "family guy 2", description: "funny funny")
-      south_park_2 = Video.create(title: "south park 2", description: "south park" )
-      family_guy_3 = Video.create(title: "family guy 3", description: "funny funny")
-      south_park_3 = Video.create(title: "south park 3", description: "south park" )
-      get :search, search: "family"
-      assigns(:videos).should == [family_guy, family_guy_2, family_guy_3]
+    before do
+      session[:user_id] = Fabricate(:user).id
+      6.times { Fabricate(:video)}
+      @video = Fabricate(:video)
+    end
 
+    it "set the @videos variable" do
+      get :search, search: @video.title
+      assigns(:videos).should == [@video]
     end
     
     it "render the search template" do
