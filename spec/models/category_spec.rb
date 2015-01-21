fc5c097238a1a5dfea19e230e0549da1523b2ab4
Gtar <chihweichang69@gmail.com>
2015-01-21 12:33:18 +0800

    categroy_spec using rubyist style => each

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,26 +4,24 @@ describe Category do
   it { should have_many(:videos)}
 
   describe "#recent_videos" do
-
-    it "returns 6 videos if there are more than 6 videos in the category" do
-      comedies = Category.create(name: "comedies")
-      for i in 1..8
-        vvv = Video.create(title: "family guy"+i.to_s ,description: 
-          "family guy test! ! !", category: comedies)
+    let(:videos) do
+      @comedies = Category.create(name: "comedies")
+      @videos = []
+      (1..8).to_a.each do |i|  
+        @videos[i] = Video.create(title: "family guy"+i.to_s ,description: 
+          "family guy test! ! !", category: @comedies)
       end
-      expect(comedies.recent_videos.count).to eq(6)
     end
 
-    it "returns 6 videos in desc order" do 
-      comedies = Category.create(name: "comedies")
-      videos = []
-      for i in 0..8
-        videos[i] = Video.create(title: "family guy"+i.to_s ,description: 
-          "family guy test! ! !", category: comedies, created_at: Time.now+(60*i) )
-      end
-      
-      expect(comedies.recent_videos).to eq([videos[8],videos[7],videos[6],
-        videos[5],videos[4],videos[3]])
+    it "returns 6 videos if there are more than 6 videos in the category" do
+      videos
+      expect(@comedies.recent_videos.count).to eq(6)
+    end
+
+    it "returns 6 videos in desc order" do
+      videos
+      expect(@comedies.recent_videos).to eq([@videos[8],@videos[7],@videos[6],
+        @videos[5],@videos[4],@videos[3]])
     end
 
   end
